c661c7c8df671462a8e355755b42cf2ee3867090
hakyeoung <johndoe@example.com>
2026-05-18 22:16:05 +0900

    fix: gradle 버전충돌 해결완료

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.dailymoodcare"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.dailymoodcare"
         minSdk = 30
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
